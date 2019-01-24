441d1a119ded394d38da3e1247329de8808c8e1f
humuhimi <masa19951009@gmail.com>
2019-01-24 23:25:37 +0800

    try boxplot with python plus a

--- Lesson1/boxplot.py
+++ Lesson1/boxplot.py
@@ -16,5 +16,23 @@ if __name__ == '__main__':
 
 box_plot(data)
 
+#Creates just a figure and only one subplot
+# fig, ax = plt.subplots()
+# ax.plot(x, y)
+# ax.set_title('Simple plot')
+
+'''
+if more datas need
+
+a = np.random.normal(100, 10, 200)
+b = np.random.normal(80, 30, 200)
+c = np.random.normal(90, 20, 200)
+d = np.random.normal(70, 25, 200)
+
+fig, ax = plt.subplots()
+ax.boxplot([a, b, c, d], labels=['A', 'B', 'C', 'D'])
+'''
+
+
 
 
